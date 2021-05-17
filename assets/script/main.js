40ae730d526910748d5cc2252da97d0f76d68ee6
Famahub <famagueye@gmail.com>
2021-05-17 09:22:19 +0200

    filtre barre de recherche

---
+++ assets/script/main.js
@@ -0,0 +1,23 @@
+// Barre de recherche jQuery
+
+ const search = document.getElementById("search");
+const productName = document.querySelectorAll(".article h4");
+
+search.addEventListener("keyup", searchBar);
+
+
+function searchBar(e) {
+    const text = e.target.value.toLowerCase();
+    // console.log(productName[0]);
+    productName.forEach(function(product) {
+        const item = product.firstChild.textContent;
+        if (item.toLowerCase().indexOf(text) != -1) {
+            product.parentElement.parentElement.style.display = "block"
+        } else {
+            product.parentElement.parentElement.style.display = "none"
+        }
+    })
+}
+
+
+
